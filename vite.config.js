1792aa1d297a58f50938d15290127b478eecaaa8
Ikarows <1254803307@qq.com>
2022-05-21 16:23:59 +0800

    优化vite.cofnigs配置

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,9 @@ import styleImport from 'vite-plugin-style-import'
 
 // import.meta.VITE_APP_ENV
 export default defineConfig({
-  outputDir: 'dist',
-  assetsDir: 'public',
+  base: './',
+  // outputDir: 'dist',
+  // assetsDir: 'public',
   productionSourceMap: false,
   chainWebpack(config) {
     config.plugins.delete('preload')
@@ -35,19 +36,19 @@ export default defineConfig({
     assetsDir: 'public',
     outDir: `dist`
   },
-  hostname: 'localhost', // 默认是 localhost
-  port: '3000', // 默认是 3000 端口
-  open: false, // 浏览器自动打开
-  https: false, // 是否开启 https
-  ssr: false, // 服务端渲染
-  base: './', // 生产环境下的公共路径
-  outDir: 'dist' // 打包构建输出路径，默认 dist ，如果路径存在，构建之前会被删除
-  /*proxy: {
-    // 本地开发环境通过代理实现跨域，生产环境使用 nginx 转发
-    '/api': {
-      target: 'http://127.0.0.1:7001', // 后端服务实际地址
-      changeOrigin: true,
-      rewrite: path => path.replace(/^\/api/, '')
-    }
-  }*/
+  server: {
+    // 是否自动在浏览器打开
+    open: false,
+    // 是否开启 https
+    https: false,
+    // 服务端渲染
+    ssr: false
+    /*proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:7001',
+        changeOrigin: true,
+        rewrite: path => path.replace(/^\/api/, '')
+      }
+    }*/
+  }
 })
